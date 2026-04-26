c3b92544abb5f9fec590ea999c0bebb622376eac
fidelevaristo <fidelevaristo7@gmail.com>
2026-04-26 20:06:13 +0200

    feat(onboarding): define initial welcome steps configuration

---
+++ js/data/welcome-steps.js
@@ -0,0 +1,56 @@
+export const welcomeSteps = [
+    {
+        id: "discovery",
+        question: "Come ci hai conosciuto?",
+        options: [
+            { id: "Tiktok", label: "Tiktok" },
+            { id: "Notizie/articoli/blog", label: "Notizie/articoli/blog" },
+            { id: "Facebook/Instagram", label: "Facebook/Instagram" },
+            { id: "TV", label: "TV" },
+            { id: "Amici e famiglia", label: "Amici e famiglia" },
+            { id: "YouTube", label: "YouTube" },
+            { id: "Ricerca su Google", label: "Ricerca su Google" },
+            { id: "Altro", label: "Altro" }
+        ]
+    },
+
+
+
+    {
+        id: "goal",
+        question: "Perché vuoi imparare il portoghese?",
+        options: [
+            { id: "Migliorare la mia educazione", label: "Migliorare la mia educazione" },
+            { id: "Conoscere altre persone", label: "Conoscere altre persone" },
+            { id: "Nuove possibilità di carriera", label: "Nuove possibilità di carriera" },
+            { id: "Divertimento", label: "Divertimento" },
+            { id: "Prepararmi per un viaggio", label: "Prepararmi per un viaggio" },
+            { id: "Esercitare la mente", label: "Esercitare la mente" },
+            { id: "Altro", label: "Altro" },
+        ]
+    },
+
+    {
+        id: "level",
+        question: "Come te la cavi in portoghese?",
+        options: [
+            { id: "Ho appena iniziato a studiare portoghese", label: "Ho appena iniziato a studiare portoghese" },
+            { id: "Conosco alcune parole di uso comune", label: "Conosco alcune parole di uso comune" },
+            { id: "Riesco a sostenere conversazione di basi", label: "Riesco a sostenere conversazione di basi" },
+            { id: "Riesco a parlare di vari argomenti", label: "Riesco a parlare di vari argomenti" },
+            { id: "Riesco a parlare in detaglio della maggior parte degli argomenti", label: "Riesco a parlare in detaglio della maggior parte degli argomenti" },
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+]
