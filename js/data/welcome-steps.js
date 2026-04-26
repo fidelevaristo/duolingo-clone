83a5098d932ce2c4e09845f0721b3d7ed306275e
fidelevaristo <fidelevaristo7@gmail.com>
2026-04-26 20:44:35 +0200

    feat(onboarding-data): add welcome flow steps and options

--- js/data/welcome-steps.js
+++ js/data/welcome-steps.js
@@ -3,14 +3,14 @@ export const welcomeSteps = [
         id: "discovery",
         question: "Come ci hai conosciuto?",
         options: [
-            { id: "Tiktok", label: "Tiktok" },
-            { id: "Notizie/articoli/blog", label: "Notizie/articoli/blog" },
-            { id: "Facebook/Instagram", label: "Facebook/Instagram" },
-            { id: "TV", label: "TV" },
-            { id: "Amici e famiglia", label: "Amici e famiglia" },
-            { id: "YouTube", label: "YouTube" },
-            { id: "Ricerca su Google", label: "Ricerca su Google" },
-            { id: "Altro", label: "Altro" }
+            { id: "tiktok", label: "TikTok" },
+            { id: "notizie-articoli-blog", label: "Notizie/articoli/blog" },
+            { id: "facebook-instagram", label: "Facebook/Instagram" },
+            { id: "tv", label: "TV" },
+            { id: "amici-e-famiglia", label: "Amici e famiglia" },
+            { id: "youtube", label: "YouTube" },
+            { id: "ricerca-su-google", label: "Ricerca su Google" },
+            { id: "altro", label: "Altro" }
         ]
     },
 
@@ -20,13 +20,13 @@ export const welcomeSteps = [
         id: "goal",
         question: "Perché vuoi imparare il portoghese?",
         options: [
-            { id: "Migliorare la mia educazione", label: "Migliorare la mia educazione" },
-            { id: "Conoscere altre persone", label: "Conoscere altre persone" },
-            { id: "Nuove possibilità di carriera", label: "Nuove possibilità di carriera" },
-            { id: "Divertimento", label: "Divertimento" },
-            { id: "Prepararmi per un viaggio", label: "Prepararmi per un viaggio" },
-            { id: "Esercitare la mente", label: "Esercitare la mente" },
-            { id: "Altro", label: "Altro" },
+            { id: "migliorare-educazione", label: "Migliorare la mia educazione" },
+            { id: "conoscere-persone", label: "Conoscere altre persone" },
+            { id: "nuove-possibilita-carriera", label: "Nuove possibilità di carriera" },
+            { id: "divertimento", label: "Divertimento" },
+            { id: "prepararmi-viaggio", label: "Prepararmi per un viaggio" },
+            { id: "esercitare-la-mente", label: "Esercitare la mente" },
+            { id: "altro", label: "Altro" },
         ]
     },
 
@@ -34,11 +34,11 @@ export const welcomeSteps = [
         id: "level",
         question: "Come te la cavi in portoghese?",
         options: [
-            { id: "Ho appena iniziato a studiare portoghese", label: "Ho appena iniziato a studiare portoghese" },
-            { id: "Conosco alcune parole di uso comune", label: "Conosco alcune parole di uso comune" },
-            { id: "Riesco a sostenere conversazione di basi", label: "Riesco a sostenere conversazione di basi" },
-            { id: "Riesco a parlare di vari argomenti", label: "Riesco a parlare di vari argomenti" },
-            { id: "Riesco a parlare in detaglio della maggior parte degli argomenti", label: "Riesco a parlare in detaglio della maggior parte degli argomenti" },
+            { id: "studiare-portoghese", label: "Ho appena iniziato a studiare portoghese" },
+            { id: "Conosco-parole", label: "Conosco alcune parole di uso comune" },
+            { id: "conversazione-basi", label: "Riesco a sostenere conversazione di basi" },
+            { id: "vari-argomenti", label: "Riesco a parlare di vari argomenti" },
+            { id: "maggior-parte-degli-argomenti", label: "Riesco a parlare in detaglio della maggior parte degli argomenti" },
         ]
     }
 
---
+++ js/main/welcome.js
@@ -0,0 +1,35 @@
+import { welcomeSteps } from "../data/welcome-steps";
+import { Onboarding } from "../models/Onboarding";
+
+
+const onboarding = new Onboarding(welcomeSteps)
+
+console.log("Initial State: ")
+console.log(onboarding.getState())
+
+console.log("Trying to continue without option: ")
+try {
+    onboarding.next()
+} catch (error) {
+    console.log(error.message)
+}
+
+console.log("Selecting option: ")
+console.log(onboarding.selectOption("tiktok"))
+console.log(onboarding.getState());
+
+console.log("Going next:");
+onboarding.next();
+console.log(onboarding.getState());
+
+console.log("Selecting option in second step:");
+onboarding.selectOption("migliorare-educazione");
+console.log(onboarding.getState());
+
+console.log("Going back:");
+onboarding.back();
+console.log(onboarding.getState());
+
+console.log("Going forward again:");
+onboarding.next();
+console.log(onboarding.getState());
