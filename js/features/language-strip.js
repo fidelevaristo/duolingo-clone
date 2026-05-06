cbefb366b7a5e810686b9f109dc00cd448338636
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-06 15:29:35 +0200

    Add responsive language strip with infinite scroll navigation

--- js/features/language-strip.js
+++ js/features/language-strip.js
@@ -5,19 +5,52 @@ const prevButton = $("#languageStripPrev");
 const nextButton = $("#languageStripNext");
 
 if (languageMenu && prevButton && nextButton) {
-  const getScrollAmount = () => languageMenu.clientWidth * 0.8;
+  const getScrollAmount = () => languageMenu.clientWidth * 0.85;
 
-  on(prevButton, "click", () => {
-    languageMenu.scrollBy({
-      left: -getScrollAmount(),
+  const isAtStart = () => languageMenu.scrollLeft <= 2;
+
+  const isAtEnd = () => {
+    const maxScrollLeft = languageMenu.scrollWidth - languageMenu.clientWidth;
+    return languageMenu.scrollLeft >= maxScrollLeft - 2;
+  };
+
+  /**
+   * Scorre il menu verso una posizione specifica.
+   *
+   * @param {number} left - Posizione orizzontale dello scroll
+   */
+  function scrollToPosition(left) {
+    languageMenu.scrollTo({
+      left,
       behavior: "smooth",
     });
+  }
+
+  /**
+   * Torna all'inizio se siamo alla fine,
+   * altrimenti scorre verso destra.
+   */
+  on(nextButton, "click", () => {
+    if (isAtEnd()) {
+      scrollToPosition(0);
+      return;
+    }
+
+    scrollToPosition(languageMenu.scrollLeft + getScrollAmount());
   });
 
-  on(nextButton, "click", () => {
-    languageMenu.scrollBy({
-      left: getScrollAmount(),
-      behavior: "smooth",
-    });
+  /**
+   * Va alla fine se siamo all'inizio,
+   * altrimenti scorre verso sinistra.
+   */
+  on(prevButton, "click", () => {
+    const maxScrollLeft = languageMenu.scrollWidth - languageMenu.clientWidth;
+
+    if (isAtStart()) {
+      scrollToPosition(maxScrollLeft);
+      return;
+    }
+
+    scrollToPosition(languageMenu.scrollLeft - getScrollAmount());
   });
 }
